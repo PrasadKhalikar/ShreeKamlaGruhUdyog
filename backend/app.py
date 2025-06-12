936e9f8acc04bab92706ba8d9b199e2c913d6bfb
PrasadKhalikar <khalikarprasad@gmail.com>
2025-06-12 11:11:34 +0530

    postre update

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,8 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
